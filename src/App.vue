b3db42cb53f41b5a9a754f8cfe32a6358a884c73
Jean Paulo <jean.paulo@edu.itajai.sc.gov.br>
2023-05-12 16:09:52 -0300

    Novo projeto

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card>
+          <v-card-title class="justify-center">
+          <!-- Titulo do Projeto -->
+          </v-card-title>
+          <v-card-text>
+            <v-form >
+             <InputText texto="Nome do usúario"/>
+              <!-- Fazer Outro Componente igual InputText Porem como  senha do usúario type dele é password exemplo type="password" e label dele vai ser sua senha aqui -->
+              <!-- Aqui Vai ter um Botão usando Props Text igual InputText chamado Entrar -->
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+  </v-app>
+</template>
+
+<script>
+ import InputText from './components/InputText.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+   InputText
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/InputText.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-text-field
+       
+          :label="texto"
+          required
+        ></v-text-field>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  props: {
+    texto: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
